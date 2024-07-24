1584721b0a2540bcd86019b81a4946cd4ebc5923
akal <akalnakarmi1@gmail.com>
2024-07-24 21:17:02 +0545

    schemas

--- appPokeList/schemas.py
+++ appPokeList/schemas.py
@@ -1,1 +1,26 @@
 from pydantic import BaseModel
+
+class TypeBase(BaseModel):
+    name: str
+
+class TypeCreate(TypeBase):
+    pass
+
+class TypeRead(TypeBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class PokemonBase(BaseModel):
+    name: str
+
+class PokemonCreate(PokemonBase):
+    types: list[TypeCreate]
+
+class PokemonRead(PokemonBase):
+    id: int
+    types: list[TypeRead]
+
+    class Config:
+        orm_mode = True
